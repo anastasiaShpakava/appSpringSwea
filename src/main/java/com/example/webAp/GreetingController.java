82b5cab655f2c8baa97f8b31cddd3d17a9ac4c54
Anastasia Shpakova <shpakavaa@gmail.com>
2020-03-16 15:04:27 +0300

    add beans

--- src/main/java/com/example/webAp/GreetingController.java
+++ src/main/java/com/example/webAp/GreetingController.java
@@ -18,21 +18,25 @@ public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting(
-            @RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model) {
+            @RequestParam(name = "name", required = false, defaultValue = "World") String name, Map<String, Object> model) {
         model.put("name", name);
         return "greeting";
     }
+
     @GetMapping
-    public String main (Map <String, Object> model){
-        Iterable <Message> messages=messageRepos.findAll();
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepos.findAll();
         model.put("messages", messages);
         return "main";
     }
+
     @PostMapping
-    public String add(@RequestParam String text, @RequestParam String tag, Map <String, Object> model){
-Message message=new Message(text, tag);
-messageRepos.save(message);
-return "main";
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepos.save(message);
+        Iterable<Message> messages = messageRepos.findAll();
+        model.put("messages", messages);
+        return "main";
     }
 
 }
