82e7b0be80fd6ad57054c22ed1bfa24c98491750
Anastasia Shpakova <shpakavaa@gmail.com>
2020-03-16 15:03:31 +0300

    add beans

--- src/main/java/com/example/webAp/GreetingController.java
+++ src/main/java/com/example/webAp/GreetingController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
@@ -26,6 +27,12 @@ public class GreetingController {
         Iterable <Message> messages=messageRepos.findAll();
         model.put("messages", messages);
         return "main";
+    }
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map <String, Object> model){
+Message message=new Message(text, tag);
+messageRepos.save(message);
+return "main";
     }
 
 }
